61f3b554c0a5da936207e1ea1372f69e2e482d62
3r <valentinojuanito56@gmail.com>
2024-04-05 17:33:22 +0700

    update`

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,21 @@
- let judul = document.getElementById("judul")
- judul.textContent="Java Script"
- 
- 
+let nama = document.getElementById("nama");
+let instansi = document.getElementById("instansi");
+let simpan = document.getElementById("simpan");
+let list = document.getElementById("list");
+let data = [];
+let table = document.getElementById("table");
+
+simpan.addEventListener("click", function () {
+  console.log("p");
+  data.push({ nama: nama.value, instansi: instansi.value });
+  console.log(data);
+  list.innerHTML += " ";
+
+  for (const [index, value] of data.entries()) {
+    // list.innerHTML += `<li> Nama = ${value.nama}, Instansi ${value.instansi} </li>`;
+    table.innerHTML += `<tr>
+        <td>${value.nama}</td>
+        <td>${value.instansi}</td>
+    </tr>`;
+  }
+});
